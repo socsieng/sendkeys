409e0fbe935113329b1d61cdbe3f2cd186781117
Soc Sieng <socsieng@gmail.com>
2020-12-30 23:42:53 -0800

    feat: add check to see if accessibility permissions have been enabled

--- Sources/sendkeys/main.swift
+++ Sources/sendkeys/main.swift
@@ -1,4 +1,11 @@
 import SendKeysLib
+import Foundation
+
+let accessEnabled = AXIsProcessTrustedWithOptions([kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] as CFDictionary)
+
+if !accessEnabled {
+    fputs("\nAccessibility preferences must be enabled to use this tool. If running from the terminal, make sure that your terminal app has accessibility permissiions enabled.\n\n", stderr)
+}
 
 if #available(OSX 10.11, *) {
     SendKeysCli.main()
