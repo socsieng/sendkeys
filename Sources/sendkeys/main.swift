6043a784a9f2245bd826b30787d494a687899428
Soc Sieng <socsieng@gmail.com>
2020-12-31 00:41:58 -0800

    fix: only perform accessibility check if stdin is tty

--- Sources/sendkeys/main.swift
+++ Sources/sendkeys/main.swift
@@ -1,10 +1,16 @@
 import SendKeysLib
 import Foundation
 
-let accessEnabled = AXIsProcessTrustedWithOptions([kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] as CFDictionary)
+private func isTty() -> Bool {
+    return isatty(FileHandle.standardInput.fileDescriptor) == 1
+}
 
-if !accessEnabled {
-    fputs("\nAccessibility preferences must be enabled to use this tool. If running from the terminal, make sure that your terminal app has accessibility permissiions enabled.\n\n", stderr)
+if isTty() {
+    let accessEnabled = AXIsProcessTrustedWithOptions([kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] as CFDictionary)
+
+    if !accessEnabled {
+        fputs("\nAccessibility preferences must be enabled to use this tool. If running from the terminal, make sure that your terminal app has accessibility permissiions enabled.\n\n", stderr)
+    }
 }
 
 if #available(OSX 10.11, *) {
