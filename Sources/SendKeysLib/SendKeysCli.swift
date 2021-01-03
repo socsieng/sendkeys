670d091d17100fd9b9cdb74ba0eebf69cae6af51
Soc Sieng <socsieng@gmail.com>
2021-01-02 17:11:15 -0800

    feat: defer accesibility check to execution of the command

--- Sources/SendKeysLib/SendKeysCli.swift
+++ Sources/SendKeysLib/SendKeysCli.swift
@@ -9,36 +9,42 @@ public struct SendKeysCli: ParsableCommand {
         version: "0.0.0", /* auto-updated */
         subcommands: [MousePosition.self]
     )
-    
+
     @Option(name: .shortAndLong, help: "Name of the application to send keys to.")
     var applicationName: String?
-    
+
     @Option(name: .shortAndLong, help: "Default delay between keystrokes in seconds.")
     var delay: Double = 0.1
-    
+
     @Option(name: .shortAndLong, help: "Initial delay before sending commands in seconds.")
     var initialDelay: Double = 1
-    
+
     @Option(name: NameSpecification([.customShort("f"), .long ]), help: "File containing keystroke instructions.")
     var inputFile: String?
-    
+
     @Option(name: .shortAndLong, help: "String of characters to send.")
     var characters: String?
 
     public init() { }
 
     public mutating func run() throws {
+        let accessEnabled = AXIsProcessTrustedWithOptions([kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] as CFDictionary)
+
+        if !accessEnabled {
+            fputs("WARNING: Accessibility preferences must be enabled to use this tool. If running from the terminal, make sure that your terminal app has accessibility permissiions enabled.\n\n", stderr)
+        }
+
         if !(applicationName ?? "").isEmpty {
             try AppActivator(appName: applicationName!).activate()
         }
-        
+
         if (initialDelay > 0) {
             Sleeper.sleep(seconds: initialDelay)
         }
-        
+
         let commandProcessor = CommandsProcessor(defaultPause: delay)
         var commandString: String?
-        
+
         if !(inputFile ?? "").isEmpty {
             if let data = FileManager.default.contents(atPath: inputFile!) {
                 commandString = String(data: data, encoding: .utf8)
@@ -48,12 +54,12 @@ public struct SendKeysCli: ParsableCommand {
         } else if !(characters ?? "").isEmpty {
             commandString = characters
         }
-        
+
         if !(commandString ?? "").isEmpty {
             commandProcessor.process(commandString!)
         } else if !isTty() {
             var data: Data
-            
+
             repeat {
                 data = FileHandle.standardInput.availableData
 
