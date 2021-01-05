2e68f08a6016e084ac86d77e27e284d4209c6bff
Soc Sieng <socsieng@gmail.com>
2021-01-04 17:13:06 -0800

    build: fix typo in bottle.sh

--- scripts/bottle.sh
+++ scripts/bottle.sh
@@ -5,8 +5,8 @@ set -e
 cwd=`pwd`
 script_folder=`cd $(dirname $0) && pwd`
 version=${1:-`cat $script_folder/../version.txt`}
-forumula_template=$script_folder/../Formula/sendkeys_template.rb
-forumula=$script_folder/../Formula/sendkeys.rb
+formula_template=$script_folder/../Formula/sendkeys_template.rb
+formula=$script_folder/../Formula/sendkeys.rb
 url="file://$cwd/sendkeys.tar.gz"
 sed_url=`echo $url | sed 's/\\//\\\\\//g'`
 
@@ -14,15 +14,15 @@ version=`echo $version | sed -E 's/^v//g'`
 
 tar zcvf sendkeys.tar.gz --exclude=".git" --exclude=".build" ./
 
-cp $forumula_template $forumula
+cp $formula_template $formula
 
 # update url
-sed -E -i "" "s/url \"\"/url \"$sed_url\"/g" $forumula
+sed -E -i "" "s/url \"\"/url \"$sed_url\"/g" $formula
 
 # update version number
-sed -E -i "" "s/version \"[0-9]+\.[0-9]+\.[0-9]+\"/version \"$version\"/g" $forumula
+sed -E -i "" "s/version \"[0-9]+\.[0-9]+\.[0-9]+\"/version \"$version\"/g" $formula
 
-brew install --force --build-bottle $forumula
+brew install --force --build-bottle $formula
 brew bottle sendkeys --force-core-tap --root-url "https://github.com/socsieng/sendkeys/releases/download/v${version}"
 
 bottle=`ls sendkeys--$version.*.tar.gz`
