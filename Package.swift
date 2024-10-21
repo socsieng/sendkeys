b1e1319259cef524e5c83632eabecdbcd353b0bb
Soc Sieng <socsieng@gmail.com>
2024-10-22 00:34:01 +1100

    fix: update package versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,13 +7,13 @@ let package = Package(
     name: "sendkeys",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.1"),
-        .package(url: "https://github.com/apple/swift-format.git", .branch("swift-5.3-branch")),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-format", from: "600.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+        .executableTarget(
             name: "sendkeys",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
