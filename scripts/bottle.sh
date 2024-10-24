e38451aad1f9a2b8c8d4ab5afb25c9de26147d8f
Soc Sieng <socsieng@gmail.com>
2024-10-24 14:32:49 +1100

    fix(build): restore arm64 binary in homebrew formula

--- scripts/bottle.sh
+++ scripts/bottle.sh
@@ -5,7 +5,6 @@ set -e
 cwd=`pwd`
 script_folder=`cd $(dirname $0) && pwd`
 version=$1
-target=$2
 formula_template=$script_folder/../Formula/sendkeys_template.rb
 formula=$script_folder/../Formula/sendkeys.rb
 url="file://$cwd/sendkeys.tar.gz?date=`date +%s`"
@@ -31,13 +30,16 @@ echo "Bottle built"
 brew bottle sendkeys --force-core-tap --no-rebuild --root-url "https://github.com/socsieng/sendkeys/releases/download/v${version}"
 
 bottle=`ls sendkeys--$version.*.tar.gz`
-bottle_rename="sendkeys-$version.$target.bottle.tar.gz"
+bottle_big_sur="sendkeys-$version.big_sur.bottle.tar.gz"
+bottle_arm64_big_sur="sendkeys-$version.arm64_big_sur.bottle.tar.gz"
 
-mv $bottle $bottle_rename
+cp $bottle $bottle_big_sur
+cp $bottle $bottle_arm64_big_sur
 
-echo "file=$bottle_rename" >> $GITHUB_OUTPUT
+echo "big_sur=$bottle_big_sur" >> $GITHUB_OUTPUT
+echo "arm64_big_sur=$bottle_arm64_big_sur" >> $GITHUB_OUTPUT
 echo "root_url=https://github.com/socsieng/sendkeys/releases/download/v${version}" >> $GITHUB_OUTPUT
 echo "url=https://github.com/socsieng/sendkeys/releases/download/v${version}/$bottle_rename" >> $GITHUB_OUTPUT
-echo "sha=$(shasum -a 256 $bottle_rename | awk '{printf $1}')" >> $GITHUB_OUTPUT
+echo "sha=$(shasum -a 256 $bottle | awk '{printf $1}')" >> $GITHUB_OUTPUT
 
 brew uninstall sendkeys
