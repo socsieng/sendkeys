3b651bfdd9696c52e82a6ca5bd49b14f42193dfc
Soc Sieng <socsieng@gmail.com>
2024-10-28 23:54:41 +1100

    chore: add link to readme file in help

--- Sources/SendKeysLib/SendKeysCli.swift
+++ Sources/SendKeysLib/SendKeysCli.swift
@@ -5,7 +5,8 @@ import Foundation
 public struct SendKeysCli: ParsableCommand {
     public static let configuration = CommandConfiguration(
         commandName: "sendkeys",
-        abstract: "Command line tool for automating keystrokes and mouse events.",
+        abstract:
+            "Command line tool for automating keystrokes and mouse events. More information: https://github.com/socsieng/sendkeys/blob/main/README.md",
         version: "0.0.0", /* auto-updated */
         subcommands: [Sender.self, AppLister.self, MousePosition.self, Transformer.self],
         defaultSubcommand: Sender.self
